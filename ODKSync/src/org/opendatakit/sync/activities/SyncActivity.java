9697094d24f40928990d672d02c2da96f79da12f
mitchellsundt <mitchellsundt@gmail.com>
2014-08-05 12:14:35 -0700

    Do not upload the tables.init file to the server (but also do not remove it).

---
+++ ODKSync/src/org/opendatakit/sync/activities/SyncActivity.java
@@ -0,0 +1,439 @@
+/*
+ * Copyright (C) 2012 University of Washington
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.opendatakit.sync.activities;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.opendatakit.sync.OdkSyncServiceProxy;
+import org.opendatakit.sync.R;
+import org.opendatakit.sync.SyncConsts;
+import org.opendatakit.sync.SyncPreferences;
+import org.opendatakit.sync.exceptions.NoAppNameSpecifiedException;
+import org.opendatakit.sync.files.SyncUtil;
+import org.opendatakit.sync.service.SyncProgressState;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * An activity for downloading from and uploading to an ODK Aggregate instance.
+ * 
+ */
+public class SyncActivity extends Activity {
+
+  private static final String LOGTAG = SyncActivity.class.getSimpleName();
+
+  private static final String ACCOUNT_TYPE_G = "com.google";
+  private static final String URI_FIELD_EMPTY = "http://";
+
+  private static final int AUTHORIZE_ACCOUNT_RESULT_ID = 1;
+
+  private static final AtomicBoolean refreshRequired = new AtomicBoolean(false);
+  
+  public static final void refreshActivityUINeeded() {
+    refreshRequired.set(true);
+  }
+
+  private EditText uriField;
+  private Spinner accountListSpinner;
+
+  private String appName;
+  private AccountManager accountManager;
+
+  private OdkSyncServiceProxy syncProxy;
+
+  private TextView progressState;
+  private TextView progressMessage;
+
+  private UpdateStatusTask updateStatusTask;
+  
+  private boolean authorizeAccountSuccessful;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    appName = getIntent().getStringExtra(SyncConsts.INTENT_KEY_APP_NAME);
+    if (appName == null) {
+      appName = SyncUtil.getDefaultAppName();
+    }
+    accountManager = AccountManager.get(this);
+
+    syncProxy = new OdkSyncServiceProxy(this);
+
+    setTitle("ODK SYNC");
+    setContentView(R.layout.aggregate_activity);
+    findViewComponents();
+    try {
+      SyncPreferences prefs = new SyncPreferences(this, appName);
+      initializeData(prefs);
+      updateButtonsEnabled(prefs);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    authorizeAccountSuccessful = false;
+
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    try {
+      SyncPreferences prefs = new SyncPreferences(this, appName);
+      updateButtonsEnabled(prefs);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    try {
+      SyncPreferences prefs = new SyncPreferences(this, appName);
+      updateButtonsEnabled(prefs);
+      updateStatusTask = new UpdateStatusTask();
+      updateStatusTask.execute();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
+  @Override
+  protected void onPause() {
+      updateStatusTask.cancel(false);
+      updateStatusTask = null;
+      super.onPause();
+  }
+
+  @Override
+  protected void onDestroy() {
+    syncProxy.shutdown();
+    super.onDestroy();
+  }
+
+  private void findViewComponents() {
+    uriField = (EditText) findViewById(R.id.aggregate_activity_uri_field);
+    accountListSpinner = (Spinner) findViewById(R.id.aggregate_activity_account_list_spinner);
+    progressState = (TextView) findViewById(R.id.aggregate_activity_progress_state);
+    progressMessage = (TextView) findViewById(R.id.aggregate_activity_progress_message);
+  }
+
+  private void initializeData(SyncPreferences prefs) {
+    // Add accounts to spinner
+    Account[] accounts = accountManager.getAccountsByType(ACCOUNT_TYPE_G);
+    List<String> accountNames = new ArrayList<String>(accounts.length);
+    for (int i = 0; i < accounts.length; i++)
+      accountNames.add(accounts[i].name);
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.select_dialog_item, accountNames);
+    accountListSpinner.setAdapter(adapter);
+
+    // Set saved server url
+    String serverUri = prefs.getServerUri();
+
+    if (serverUri == null)
+      uriField.setText(URI_FIELD_EMPTY);
+    else
+      uriField.setText(serverUri);
+
+    // Set chosen account
+    String accountName = prefs.getAccount();
+    if (accountName != null) {
+      int index = accountNames.indexOf(accountName);
+      accountListSpinner.setSelection(index);
+    }
+  }
+
+  void updateButtonsEnabled(SyncPreferences prefs) {
+    String accountName = prefs.getAccount();
+    String serverUri = prefs.getServerUri();
+
+    boolean haveSettings = (accountName != null) && (serverUri != null);
+
+    boolean restOfButtons = haveSettings && authorizeAccountSuccessful;
+
+    findViewById(R.id.aggregate_activity_save_settings_button).setEnabled(true);
+    findViewById(R.id.aggregate_activity_authorize_account_button).setEnabled(true);
+    findViewById(R.id.aggregate_activity_sync_now_push_button).setEnabled(restOfButtons);
+    findViewById(R.id.aggregate_activity_sync_now_pull_button).setEnabled(restOfButtons);
+  }
+
+  AlertDialog.Builder buildOkMessage(String title, String message) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setCancelable(false);
+    builder.setPositiveButton(getString(R.string.ok), null);
+    builder.setTitle(title);
+    builder.setMessage(message);
+    return builder;
+  }
+
+  /**
+   * Hooked up to save settings button in aggregate_activity.xml
+   */
+  public void onClickSaveSettings(View v) {
+    // show warning message
+    AlertDialog.Builder msg = buildOkMessage(getString(R.string.confirm_change_settings),
+        getString(R.string.change_settings_warning));
+
+    msg.setPositiveButton(getString(R.string.save), new OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+
+        try {
+          SyncPreferences prefs = new SyncPreferences(SyncActivity.this, appName);
+          // save fields in preferences
+          String uri = uriField.getText().toString();
+          if (uri.equals(URI_FIELD_EMPTY))
+            uri = null;
+          String accountName = (String) accountListSpinner.getSelectedItem();
+
+          prefs.setServerUri(uri);
+          prefs.setAccount(accountName);
+          // SS Oct 15: clear the auth token here.
+          // TODO if you change a user you can switch to their privileges
+          // without this.
+          Log.d(LOGTAG, "[onClickSaveSettings][onClick] invalidated authtoken");
+          invalidateAuthToken(SyncActivity.this, appName);          
+          updateButtonsEnabled(prefs);
+        } catch (NoAppNameSpecifiedException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+
+    msg.setNegativeButton(getString(R.string.cancel), null);
+    msg.show();
+
+  }
+
+  /**
+   * Hooked up to authorizeAccountButton's onClick in aggregate_activity.xml
+   */
+  public void onClickAuthorizeAccount(View v) {
+    try {
+      Log.d(LOGTAG, "[onClickAuthorizeAccount] invalidated authtoken");
+      invalidateAuthToken(SyncActivity.this, appName);
+      SyncPreferences prefs = new SyncPreferences(this, appName);
+      Intent i = new Intent(this, AccountInfoActivity.class);
+      Account account = new Account(prefs.getAccount(), ACCOUNT_TYPE_G);
+      i.putExtra(SyncConsts.INTENT_KEY_APP_NAME, appName);
+      i.putExtra(AccountInfoActivity.INTENT_EXTRAS_ACCOUNT, account);
+      startActivityForResult(i, AUTHORIZE_ACCOUNT_RESULT_ID);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Hooked to syncNowButton's onClick in aggregate_activity.xml
+   */
+  public void onClickSyncNowPush(View v) {
+    Log.d(LOGTAG, "in onClickSyncNowPush");
+    // ask whether to sync app files and table-level files
+    
+    // show warning message
+    AlertDialog.Builder msg = buildOkMessage(getString(R.string.confirm_reset_app_server),
+        getString(R.string.reset_app_server_warning));
+
+    msg.setPositiveButton(getString(R.string.reset), new OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        try {
+          SyncPreferences prefs = new SyncPreferences(SyncActivity.this, appName);
+          String accountName = prefs.getAccount();
+          Log.e(LOGTAG, "[onClickSyncNowPush] timestamp: " + System.currentTimeMillis());
+          if (accountName == null) {
+            Toast.makeText(SyncActivity.this, getString(R.string.choose_account), Toast.LENGTH_SHORT).show();
+          } else {
+            try {
+              syncProxy.pushToServer(appName);
+            } catch (RemoteException e) {
+              Log.e(LOGTAG, "Problem with push command");
+            }
+    
+          }
+          updateButtonsEnabled(prefs);
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+    });
+    
+    msg.setNegativeButton(getString(R.string.cancel), null);
+    msg.show();
+  }
+
+  /**
+   * Hooked to syncNowButton's onClick in aggregate_activity.xml
+   */
+  public void onClickSyncNowPull(View v) {
+    Log.d(LOGTAG, "in onClickSyncNowPull");
+    // ask whether to sync app files and table-level files
+    try {
+      SyncPreferences prefs = new SyncPreferences(this, appName);
+      String accountName = prefs.getAccount();
+      Log.e(LOGTAG, "[onClickSyncNowPull] timestamp: " + System.currentTimeMillis());
+      if (accountName == null) {
+        Toast.makeText(this, getString(R.string.choose_account), Toast.LENGTH_SHORT).show();
+      } else {
+        try {
+          syncProxy.synchronizeFromServer(appName);
+        } catch (RemoteException e) {
+          Log.e(LOGTAG, "Problem with pull command");
+        }
+      }
+      updateButtonsEnabled(prefs);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+  }
+
+  public static void invalidateAuthToken(Context context, String appName) {
+    try {
+      SyncPreferences prefs = new SyncPreferences(context, appName);
+      AccountManager.get(context).invalidateAuthToken(ACCOUNT_TYPE_G, prefs.getAuthToken());
+      prefs.setAuthToken(null);
+      refreshActivityUINeeded();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    refreshActivityUINeeded();
+    
+    // Check that the result was ok for the authorize 
+    if (requestCode == AUTHORIZE_ACCOUNT_RESULT_ID && resultCode == Activity.RESULT_OK) {
+      this.authorizeAccountSuccessful = true;
+    }
+  }
+
+  private void updateProgress() {
+    runOnUiThread(new Runnable() {
+      public void run() {
+
+        try {
+
+          SyncProgressState progress = syncProxy.getSyncProgress(appName);
+          if (progressState != null) {
+            if (progress == null) {
+              progressState.setText("NULL");
+            } else {
+              progressState.setText(progress.name());
+              
+              if (progress == SyncProgressState.COMPLETE || progress == SyncProgressState.ERROR) {
+                if (authorizeAccountSuccessful == true) {
+                  authorizeAccountSuccessful = false;
+                  refreshActivityUINeeded();
+                  Log.i(LOGTAG, "reset authorizeAccountSuccessful");
+                }
+              }
+            }
+          } else {
+            Log.e(LOGTAG, "NULL progressState variable");
+          }
+
+          String msg = syncProxy.getSyncUpdateMessage(appName);
+          if (progressMessage != null) {
+            if (progress == null) {
+              progressMessage.setText("NULL");
+            } else {
+              progressMessage.setText(msg);
+            }
+          } else {
+            Log.e(LOGTAG, "NULL progressMessage variable");
+          }
+        } catch (RemoteException e) {
+          Log.e(LOGTAG, "Problem with update messages");
+        } catch (Exception e) {
+          e.printStackTrace();
+          Log.e(LOGTAG, "in runnable for updateProgress");
+        }
+      }
+    });
+
+  }
+
+  private class UpdateStatusTask extends AsyncTask<Void, Void, Void> {
+
+    private static final int DELAY_PROGRESS_REFRESH = 2000;
+    
+    @Override
+    protected Void doInBackground(Void... params) {
+
+      try {
+        while (!isCancelled()) {
+          Thread.sleep(DELAY_PROGRESS_REFRESH);
+          updateProgress();
+
+          // check to see if another class has requested the activity to
+          // redraw the UI, for now update the buttons
+          if (refreshRequired.get()) {
+            runOnUiThread(new Runnable() {
+              public void run() {
+                try {
+                  SyncPreferences prefs = new SyncPreferences(SyncActivity.this, appName);
+                  updateButtonsEnabled(prefs);
+                } catch (Exception e) {
+                  e.printStackTrace();
+                }
+              }
+            });
+            refreshRequired.set(false);
+          }
+        }
+      } catch (InterruptedException e) {
+        Log.i(LOGTAG, "Thread interrupt exception");
+      }
+      return null;
+
+    }
+  }
+
+}
---
+++ ODKSync/src/org/opendatakit/sync/service/AppSynchronizer.java
@@ -0,0 +1,223 @@
+package org.opendatakit.sync.service;
+
+import java.util.Arrays;
+
+import org.opendatakit.sync.R;
+import org.opendatakit.sync.SyncApp;
+import org.opendatakit.sync.SyncPreferences;
+import org.opendatakit.sync.SyncProcessor;
+import org.opendatakit.sync.SynchronizationResult;
+import org.opendatakit.sync.SynchronizationResult.Status;
+import org.opendatakit.sync.Synchronizer;
+import org.opendatakit.sync.TableResult;
+import org.opendatakit.sync.activities.SyncActivity;
+import org.opendatakit.sync.aggregate.AggregateSynchronizer;
+import org.opendatakit.sync.exceptions.InvalidAuthTokenException;
+import org.opendatakit.sync.exceptions.NoAppNameSpecifiedException;
+
+import android.app.Service;
+import android.content.Context;
+import android.util.Log;
+
+public class AppSynchronizer {
+
+  private static final String LOGTAG = AppSynchronizer.class.getSimpleName();
+
+  private final Service service;
+  private final String appName;
+  private final GlobalSyncNotificationManager globalNotifManager;
+
+  private SyncStatus status;
+  private Thread curThread;
+  private SyncTask curTask;
+  private SyncNotification syncProgress;
+
+  AppSynchronizer(Service srvc, String appName, GlobalSyncNotificationManager notificationManager) {
+    this.service = srvc;
+    this.appName = appName;
+    this.status = SyncStatus.INIT;
+    this.curThread = null;
+    this.globalNotifManager = notificationManager;
+    this.syncProgress = new SyncNotification(srvc, appName);
+  }
+
+  public boolean synchronize(boolean push) {
+    if (curThread == null || (!curThread.isAlive() || curThread.isInterrupted())) {
+      curTask = new SyncTask(service, push);
+      curThread = new Thread(curTask);
+      status = SyncStatus.SYNCING;
+      curThread.start();
+      return true;
+    }
+    return false;
+  }
+
+  public SyncStatus getStatus() {
+    return status;
+  }
+
+  public String getSyncUpdateText() {
+    return syncProgress.getUpdateText();
+  }
+
+  public SyncProgressState getProgressState() {
+    return syncProgress.getProgressState();
+  }
+
+  private class SyncTask implements Runnable {
+
+    private Context cntxt;
+    private boolean push;
+
+    public SyncTask(Context context, boolean push) {
+      this.cntxt = context;
+      this.push = push;
+    }
+
+    @Override
+    public void run() {
+
+      try {
+
+        // android.os.Debug.waitForDebugger();
+
+        globalNotifManager.startingSync(appName);
+        syncProgress.updateNotification(SyncProgressState.INIT, cntxt.getString(R.string.starting_sync), 100, 0, false);
+        sync(syncProgress);
+      } catch (NoAppNameSpecifiedException e) {
+        e.printStackTrace();
+        status = SyncStatus.NETWORK_ERROR;
+        syncProgress.updateNotification(SyncProgressState.ERROR, "There were failures..." , 100, 0, false);
+      } finally {
+        try {
+          globalNotifManager.stoppingSync(appName);
+        } catch (NoAppNameSpecifiedException e) {
+          // impossible to get here
+        }
+        SyncActivity.refreshActivityUINeeded();
+      }
+
+    }
+
+    private void sync(SyncNotification syncProgress) {
+
+      SyncPreferences prefs = null;
+      try {
+        prefs = new SyncPreferences(cntxt, appName);
+      } catch (Exception e1) {
+        e1.printStackTrace();
+      }
+
+      if(prefs == null) {
+        status = SyncStatus.FILE_ERROR;
+        syncProgress.finalErrorNotification("Unable to open SyncPreferences");
+        return;
+      }
+
+      try {
+
+        Log.i(LOGTAG, "APPNAME IN SERVICE: " + appName);
+        Log.i(LOGTAG, "TOKEN IN SERVICE:" + prefs.getAuthToken());
+        Log.i(LOGTAG, "URI IN SEVERICE:" + prefs.getServerUri());
+
+        // TODO: should use the APK manager to search for org.opendatakit.N
+        // packages, and collect N:V strings e.g., 'survey:1', 'tables:1',
+        // 'scan:1' etc. where V is the > 100's digit of the version code.
+        // The javascript API and file representation are the 100's and
+        // higher place in the versionCode. N is the next package in the
+        // package chain.
+        // TODO: Future: Add config option to specify a list of other APK
+        // prefixes to the set of APKs to discover (e.g., for 3rd party
+        // app support).
+        //
+        // NOTE: server limits this string to 10 characters
+        // For now, assume all APKs are sync'd to the same API version.
+        String versionCode = SyncApp.getInstance().getVersionCodeString();
+        String odkClientVersion = versionCode.substring(0, versionCode.length() - 2);
+
+        Synchronizer synchronizer = new AggregateSynchronizer(appName, odkClientVersion,
+            prefs.getServerUri(), prefs.getAuthToken());
+        SyncProcessor processor = new SyncProcessor(cntxt, appName, synchronizer, syncProgress);
+
+        status = SyncStatus.SYNCING;
+
+        // sync the app-level files, table schemas and table-level files
+        processor.synchronizeConfigurationAndContent(push);
+
+        // and now sync the data rows. This does not proceed if there
+        // was an app-level sync failure or if the particular tableId
+        // experienced a table-level sync failure in the preceeding step.
+
+        processor.synchronizeDataRowsAndAttachments();
+
+        boolean authProblems = false;
+
+        String reason = "none";
+        // examine results
+        SynchronizationResult overallResults = processor.getOverallResults();
+        if ( overallResults.getAppLevelStatus() != Status.SUCCESS ) {
+          authProblems = (overallResults.getAppLevelStatus() == Status.AUTH_EXCEPTION);
+          reason = "overall results";
+          status = SyncStatus.NETWORK_ERROR;
+        }
+
+        for (TableResult result : overallResults.getTableResults()) {
+          org.opendatakit.sync.SynchronizationResult.Status tableStatus = result.getStatus();
+          // TODO: decide how to handle the status
+          if (tableStatus != Status.SUCCESS) {
+            if(tableStatus == Status.AUTH_EXCEPTION) {
+              authProblems = true;
+            } else if(tableStatus == Status.TABLE_PENDING_ATTACHMENTS) {
+              continue;
+            } else if(tableStatus == Status.TABLE_CONTAINS_CONFLICTS || tableStatus == Status.TABLE_REQUIRES_APP_LEVEL_SYNC) {
+              status = SyncStatus.CONFLICT_RESOLUTION;
+            } else {
+              status = SyncStatus.NETWORK_ERROR;
+              reason = "table " + result.getTableDisplayName();
+            }
+          }
+        }
+
+        if ( authProblems ) {
+          throw new InvalidAuthTokenException("Synthesized");
+        }
+
+        // if rows aren't successful, fail.
+        if (status != SyncStatus.SYNCING && status != SyncStatus.CONFLICT_RESOLUTION) {          
+          syncProgress.finalErrorNotification("There were failures. Status: " + status + " Reason:" + reason);
+          return;
+        }
+
+        // success
+        if (status != SyncStatus.CONFLICT_RESOLUTION) {
+          status = SyncStatus.SYNC_COMPLETE;
+          syncProgress.clearNotification();
+        } else {
+          syncProgress.finalErrorNotification("Conflicts exist.  Please resolve.");
+        }
+        
+       
+        Log.i(LOGTAG, "[SyncThread] timestamp: " + System.currentTimeMillis());
+      } catch (InvalidAuthTokenException e) {
+        SyncActivity.invalidateAuthToken(cntxt, appName);
+        status = SyncStatus.AUTH_RESOLUTION;
+        syncProgress.finalErrorNotification("Account Re-Authorization Required");
+      } catch (Exception e) {
+        Log.i(
+            LOGTAG,
+            "[exception during synchronization. stack trace:\n"
+                + Arrays.toString(e.getStackTrace()));
+        String msg = e.getLocalizedMessage();
+        if ( msg == null ) {
+          msg = e.getMessage();
+        }
+        if ( msg == null ) {
+          msg = e.toString();
+        }
+        status = SyncStatus.NETWORK_ERROR;
+        syncProgress.finalErrorNotification("Failed Sync: " + msg);
+      }
+    }
+
+  }
+}
