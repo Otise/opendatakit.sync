0abf1faa634ec8377fae5d98824ec7048cda6c2f
mitchellsundt <mitchellsundt@gmail.com>
2015-01-14 13:10:26 -0800

    Move DbShimService and WebServer into ODK Sync. Change to common UID.

---
+++ ODKSyncTest/src/org/opendatakit/sync/service/SyncServiceTest.java
@@ -0,0 +1,38 @@
+package org.opendatakit.sync.service;
+
+import org.opendatakit.sync.AbstractSyncServiceTest;
+
+
+public class SyncServiceTest extends AbstractSyncServiceTest {
+
+	public SyncServiceTest() {
+		super();
+	}
+
+
+	public void testBinding() {
+		OdkSyncServiceInterface odkSyncServiceInterface = bindToService();
+
+		assertStatusCorrect(odkSyncServiceInterface, SyncStatus.INIT);
+
+	}
+
+	public void testFailureForCheckins() {
+	  assertTrue(true);
+	}
+	
+	
+//	public void testRunning() {
+//		setupService();
+//
+//		try {
+//			OdkSyncServiceInterface odkSyncServiceInterface = bindToService();
+//			odkSyncServiceInterface.synchronize();
+//			assertStatusCorrect(odkSyncServiceInterface, SyncStatus.SYNC_COMPLETE);
+//		} catch (RemoteException e) {
+//			assertTrue(false);
+//		}
+//		
+//		shutdownService();
+//	}
+}
